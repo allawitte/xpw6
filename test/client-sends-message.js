023e210e77b4e18fbbf0e8890fda892828fe2932
Alla Klimova <alla_88@inbox.lv>
2017-01-21 16:09:44 -0300

    empty test with send message was ok

---
+++ test/client-sends-message.js
@@ -0,0 +1,21 @@
+'use strict';
+'use strict';
+var assert = require('assert');
+var Server = require('../src/server');
+
+describe('When client sends a message from his account', function () {
+    var server = {};
+    before(function () {
+        server = new Server();
+    });
+
+    describe('I ask to send message', function () {
+        it('Server sends message', function () {
+
+        });
+
+    });
+});
+/**
+ * Created by HP on 1/21/2017.
+ */
