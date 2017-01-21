c1c24576df4a15fa61035443f75a0a5ae250f1e2
Alla Klimova <alla_88@inbox.lv>
2017-01-21 10:08:27 -0300

    empty test is running

---
+++ test/when-server-creates-first-account.js
@@ -0,0 +1,21 @@
+'use strict';
+var assert = require('assert');
+var Server = require('../src/server');
+
+describe('When server creates the first account', function () {
+    var server = {};
+
+    beforeEach(function () {
+        server = new Server();
+    });
+    describe('I ask for first account between 4 and 19 symbols long', function () {
+        it('I have a glass with 50 grams of whisky', function () {
+
+        });
+    });
+});
+
+
+/**
+ * Created by HP on 1/21/2017.
+ */
